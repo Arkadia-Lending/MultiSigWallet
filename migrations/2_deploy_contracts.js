a1db23f49f990547f27cc13c0c64c633464225f7
Nick Geoca <nickgeoca@gmail.com>
2017-10-18 19:41:30 -0500

    Changed CLI for deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,17 +4,17 @@ const MultisigWalletFactory = artifacts.require('MultiSigWalletWithDailyLimitFac
 
 module.exports = deployer => {
   const args = process.argv.slice()
-  if (process.env.DEPLOY_FACTORY){
+  if (process.env.DEPLOY_FACTORY) {
     deployer.deploy(MultisigWalletFactory)
     console.log("Factory with Daily Limit deployed")
-  } else if (args.length < 5) {
-    console.error("Multisig with daily limit requires to pass owner " +
-      "list, required confirmations and daily limit")
-  } else if (args.length < 6) {
-    deployer.deploy(MultisigWalletWithoutDailyLimit, args[3].split(","), args[4])
-    console.log("Wallet deployed")
-  } else {
-    deployer.deploy(MultisigWalletWithDailyLimit, args[3].split(","), args[4], args[5])
+  } else if (args[3] == 'noLimit') {
+    deployer.deploy(MultisigWalletWithoutDailyLimit, args[4].split(","), args[5])
+    console.log("Wallet with no daily limit deployed")
+  } else if (args[3] == 'withLimit') {
+    deployer.deploy(MultisigWalletWithDailyLimit, args[4].split(","), args[5], args[6])
     console.log("Wallet with Daily Limit deployed")
+  } else {
+    console.error("Multisig with daily limit requires to pass owner " +
+                  "list, required confirmations and daily limit")
   }
 }
